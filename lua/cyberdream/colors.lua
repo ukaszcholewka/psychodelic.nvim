c38dd7ccb0f7546a564c89ae6cb85c8c7eec365a
Lukasz Cholewka <lukaszcholewka@loftyworks.com>
2025-04-15 12:36:32 +0200

    fasj;dfk

--- lua/cyberdream/colors.lua
+++ lua/cyberdream/colors.lua
@@ -38,20 +38,20 @@ M.default = {
 }
 
 M.light = {
-  bg = "#ffffff",
-  bg_alt = "#eaeaea",
-  bg_highlight = "#acacac",
-  fg = "#16181a",
-  grey = "#7b8496",
-  blue = "#0057d1",
-  green = "#008b0c",
-  cyan = "#008c99",
-  red = "#d11500",
-  yellow = "#997b00",
-  magenta = "#d100bf",
-  pink = "#f40064",
-  orange = "#d17c00",
-  purple = "#a018ff",
+  bg = "#16181a",
+  bg_alt = "#1e2124",
+  bg_highlight = "#6b7280",
+  fg = "#ffffff",
+  grey = "#c6cad2",
+  blue = "#03A696",
+  green = "#04BF8A",
+  cyan = "#5ef1ff",
+  red = "#ff6e5e",
+  yellow = "#f1ff5e",
+  magenta = "#ff5ef1",
+  pink = "#ff5ea0",
+  orange = "#ffbd5e",
+  purple = "#270140",
 }
 
 return M
